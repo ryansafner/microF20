95236ecb0d30ba38a77123c02796df34b58097c6
Ryan Safner <ryansafner@gmail.com>
2020-11-16 13:57:29 -0500

    Add 4.5 pdf

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,2 +1,2 @@
 # make-pdfs
-pagedown::chrome_print("https://microf20.classes.ryansafner.com/slides/4.3-slides.html")
+pagedown::chrome_print("https://microf20.classes.ryansafner.com/slides/4.5-slides.html", timeout = 100)
